61b5fa84a307d1fd91bbf1d29f6d4951017c7554
Arunreddy15 <arunreddyb15@gmail.com>
2025-09-10 00:35:10 +0530

    Hosted Iamges added

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -2,16 +2,7 @@ import React from "react";
 import { FaLinkedinIn,FaYoutube,FaInstagram ,FaRegClock ,FaStar} from "react-icons/fa";
 import { MdOutlineArrowRightAlt,MdOutlineKeyboardArrowRight  } from "react-icons/md";
 import "./Home.css"
-import Samosa from '../images/Samosa.png'
-import Biryani from "../images/biryani.png"
-import Pizza from '../images/pizza.png'
-import Chicken from '../images/chicken.png'
-import Mutton from '../images/mutton.png'
-import Fish from '../images/fish.png'
-import Icecream from '../images/icecream.png'
-import Wings from '../images/wings.png'
 import Header from "../Header/Header";
-// import {ICECREAM,MUTTON,CHICKEN,PIZZA,SAMOSA,BIRYANI,FISH,WINGS} from '../../utils/images'
 export default function LaunchPage() {
   return (
   <>
@@ -38,7 +29,7 @@ export default function LaunchPage() {
                     </div>
                     <div className="dc-body">
                         <div className="dc-body-name">
-                            <img src={Wings} height='80'/>
+                            <img src='https://i.postimg.cc/fk4m17hr/wings.png' height='70'/>
                             <div>
                                 <h3>Chicken Wings</h3>
                                 <p>90 Calories</p>
@@ -51,15 +42,15 @@ export default function LaunchPage() {
             </div>
             <div className="content-right">
                 <span></span>
-                    <img src={Chicken} className="main-img"/>
+                    <img src='https://i.postimg.cc/9Fd5wxKr/chicken.png' className="main-img"/>
                     <p className="tagline">Let your food wait's for you from now!...</p>
                 <div className="one">
-                    <img src={Samosa} height='60' className="samosa"/>
-                    <img src={Biryani} height='60' className="biryani"/>
-                    <img src={Pizza} height='60' className="pizza"/>
-                    <img src={Mutton} height='60' className="mutton"/>
-                    <img src={Fish} height='60' className="fish"/>
-                    <img src={Icecream} height='60' className="icecream"/>
+                    <img src='https://i.postimg.cc/xCKjjKrm/samosa.png' height='60' className="samosa"/>
+                    <img src='https://i.postimg.cc/13ws9mcV/biryani.png' height='60' className="biryani"/>
+                    <img src='https://i.postimg.cc/ZR79z86L/pizza.png' height='60' className="pizza"/>
+                    <img src='https://i.postimg.cc/xCT2yKrL/mutton.png' height='60' className="mutton"/>
+                    <img src='https://i.postimg.cc/rsx4kdjZ/fish.png' height='60' className="fish"/>
+                    <img src='https://i.postimg.cc/PqMHwS7t/icecream.png' height='60' className="icecream"/>
                 </div>
             </div>
             <div className="content-right-social">
